f1eee854e5a3add7045c779629898c13f08c4b46
Harold <harold@justins-macbook-pro.local>
2018-10-29 16:59:11 -0400

    Initial Commit

---
+++ obd1Directory/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  obd1Directory
+//
+//  Created by Harold on 10/29/18.
+//  Copyright © 2018 Harold. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ obd1DirectoryTests/obd1DirectoryTests.swift
@@ -0,0 +1,36 @@
+//
+//  obd1DirectoryTests.swift
+//  obd1DirectoryTests
+//
+//  Created by Harold on 10/29/18.
+//  Copyright © 2018 Harold. All rights reserved.
+//
+
+import XCTest
+@testable import obd1Directory
+
+class obd1DirectoryTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
